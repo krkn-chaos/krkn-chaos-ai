9d8570eb0c8e1c5ff96e6d07dce3a7c7e92ee59e
Rahul Shetty <rashetty@redhat.com>
2025-05-05 17:57:30 +0530

    inital commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+## Deploying sample micro-service for testing Chaos AI
+
+```
+export DEMO_NAMESPACE=robot-shop
+export IS_OPENSHIFT=true
+./scripts/setup-demo-microservice.sh
+```
---
+++ scripts/setup-demo-microservice.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#******************************************************************************
+# File: setup-demo-microservice.sh
+# Author: Rahul Shetty (Perf & Scale)
+# Date: 2025-05-05
+#
+# Description:
+# This script sets up a sample robot-shop micro-service based app on K8s cluster.
+# 
+#
+# Usage:
+# ./setup-demo-microservice.sh
+#
+# Pre-requisite:
+# - kubectl and oc client
+# - User should be already logged into cluster before running the script
+# 
+#******************************************************************************
+
+# Define variables
+chart_name="robot-shop"
+repo_url="https://github.com/instana/robot-shop/" # Replace with the actual Helm repo URL
+namespace="${DEMO_NAMESPACE:-robot-shop}" # Replace with the desired namespace if not default
+repo_dir="temp-helm-repo"
+helm_chart="K8s/helm"
+is_openshift="${IS_OPENSHIFT:-true}" # TODO: Automatically detect if the cluster is openshift or not
+
+# Create a temporary directory
+temp_dir="./tmp"
+mkdir -p $temp_dir
+
+# Trap for exit signals and errors
+trap cleanup EXIT
+
+cleanup() {
+  echo "Script finished."
+}
+
+# Switch to the temporary directory
+cd "$temp_dir"
+echo "Switched to directory: $PWD"
+
+if [ -d "$repo_dir" ]; then
+  echo "Directory '$repo_dir' already exists. Skipping cloning."
+else
+    # Clone repository
+    echo "Directory '$repo_dir' does not exist. Cloning repository..."
+    git clone --depth 1 --branch master $repo_url $repo_dir
+fi
+
+cd $repo_dir
+echo "Switched to cloned directory: $PWD"
+
+# Setup Namespace
+kubectl get ns $namespace || kubectl create ns $namespace
+
+if [ "$is_openshift" = "true" ]; then
+    # Based on https://github.com/instana/robot-shop/tree/master/OpenShift
+    oc adm new-project $namespace
+    oc adm policy add-scc-to-user anyuid -z default -n $namespace
+    oc adm policy add-scc-to-user privileged -z default -n $namespace
+fi
+
+# Install Helm Chart
+cd $helm_chart
+helm upgrade -i $chart_name --set openshift="$is_openshift" --namespace "$namespace" .
+echo "Installed helm chart '$chart_name' in namespace '$namespace'"
+
