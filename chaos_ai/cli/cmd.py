8b942364f05b6c8c4ff1c4309dc4fc23cd298ef0
Rahul Shetty <rashetty@redhat.com>
2025-05-19 16:47:12 +0530

    update basic stuff

---
+++ chaos_ai/cli/cmd.py
@@ -0,0 +1,6 @@
+import click
+
+
+@click.command()
+def main():
+    print("DO SOMETHING 2")
---
+++ scripts/setup-krknctl.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#******************************************************************************
+# File: setup-krknctl.sh
+# Author: Rahul Shetty (Perf & Scale)
+# Date: 2025-05-13
+#
+# Description:
+# Setup Krkn CLI locally.
+# 
+#
+# Usage:
+# ./setup-krknctl.sh
+#
+# Pre-requisite:
+# - jq
+# - curl
+# 
+#******************************************************************************
+
+set -e  # Exit on error
+
+# Create temp-directory
+TMP_DIR=$(mktemp -d)
+cd $TMP_DIR
+
+cleanup() {
+  echo "Cleaning up..."
+  rm -rf "$TMP_DIR"
+}
+
+trap cleanup EXIT  # Run cleanup on exit (success or error)
+
+# Download the archive
+LATEST_TAG=$(curl -s https://api.github.com/repos/krkn-chaos/krknctl/releases/latest | jq -r .tag_name)
+echo "Downloading Krkn CLI Version: $LATEST_TAG"
+curl -LO "https://krkn-chaos.gateway.scarf.sh/krknctl-${LATEST_TAG}-linux-amd64.tar.gz"
+
+# Extract tar.gz file
+tar -xvzf "krknctl-${LATEST_TAG}-linux-amd64.tar.gz"
+
+# Move the binary to ~/.local/bin
+mkdir -p ~/.local/bin
+mv krknctl ~/.local/bin/
+chmod +x ~/.local/bin/krknctl
+
+# Verify installation
+krknctl --version
